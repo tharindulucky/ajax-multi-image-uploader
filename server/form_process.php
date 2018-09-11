04f6b522266884d4ee52b8951f9814f323cfa1e6
tharindulucky <tharindu.lakshitha.susl@gmail.com>
2018-09-12 00:33:40 +0530

    Form submission done

---
+++ server/form_process.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Ajax Multiple Image Uploader
+ * https://github.com/tharindulucky/ajax-multi-image-uploader
+ *
+ * Copyright 2018, Tharindu Lakshitha
+ * https://coderaweso.me
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ *
+ *
+ * Use this file for stuff like DB operations
+ *
+ */
+
+if(isset($_POST['submit'])){
+    die(var_dump($_POST['uploaded_image_name']));
+    //Save to DB
+    //Do whatever you want
+
+}
