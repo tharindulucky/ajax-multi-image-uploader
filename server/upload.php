c522fc5d50cd506f11c00ef226af0bb7010426ca
tharindulucky <tharindu.lakshitha.susl@gmail.com>
2018-09-13 16:32:42 +0530

    debugging

--- server/upload.php
+++ server/upload.php
@@ -30,18 +30,18 @@ if (!empty($_FILES)) {
     $file_name = $_FILES['files']['name'];
     $ext = pathinfo($file_name, PATHINFO_EXTENSION);
 
-    echo 'error';
-
     if(filesize($tempFile) > 5000000){
         echo 'FAILED';
         die();
     }
-//
-//    if($ext == 'jpg' || $ext == 'png' || $ext == 'JPG' || $ext == 'PNG'){
-//        $new_file_name = time().'.'.$ext;
-//        $targetPath = 'uploads/';  //4
-//        $targetFile =  $targetPath. $new_file_name;  //5
-//
+
+    if($ext == 'jpg' || $ext == 'png' || $ext == 'JPG' || $ext == 'PNG'){
+        $new_file_name = time().'.'.$ext;
+        $targetPath = 'uploads/';  //4
+        $targetFile =  $targetPath. $new_file_name;  //5
+
+        echo 'error';
+
 //        if ($img = @imagecreatefromstring(file_get_contents($tempFile))) {
 //            //$upload_result = move_uploaded_file($tempFile,$targetFile); //6
 //            $server_res = \Cloudinary\Uploader::upload($tempFile, array("crop"=>"limit", "tags"=>"samples", "width"=>100, "height"=>100));
@@ -49,7 +49,7 @@ if (!empty($_FILES)) {
 //        }else{
 //            echo 'FAILED';
 //        }
-//    }else{
-//        echo 'FAILED';
-//    }
+    }else{
+        echo 'FAILED';
+    }
 }
