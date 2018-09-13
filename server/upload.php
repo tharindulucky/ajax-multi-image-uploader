da75d11b7d1e059d59a54aec44c710ff0424dd14
tharindulucky <tharindu.lakshitha.susl@gmail.com>
2018-09-13 16:26:22 +0530

    debugging

--- server/upload.php
+++ server/upload.php
@@ -26,8 +26,8 @@ require 'autoload.php';
 $ds = DIRECTORY_SEPARATOR;  //1
 $storeFolder = 'uploads';   //2
 if (!empty($_FILES)) {
-    $tempFile = $_FILES['files']['tmp_name'];
-    $file_name = $_FILES['files']['name'];
+    //$tempFile = $_FILES['files']['tmp_name'];
+    //$file_name = $_FILES['files']['name'];
     //$ext = pathinfo($file_name, PATHINFO_EXTENSION);
 
     echo 'error';
