4343d25285706c314e8991441f8aeab68c56e738
tharindulucky <tharindu.lakshitha.susl@gmail.com>
2018-09-13 16:18:47 +0530

    debugging

--- server/upload.php
+++ server/upload.php
@@ -12,39 +12,39 @@
  */
 echo 'error';
 
-require 'autoload.php';
-
-\Cloudinary::config(array(
-    "cloud_name" => "dajkablj5",
-    "api_key" => "963277874729395",
-    "api_secret" => "A9TVBKOy3TKdRgsWgXx13WMFgg8"
-));
-
-$ds = DIRECTORY_SEPARATOR;  //1
-$storeFolder = 'uploads';   //2
-if (!empty($_FILES)) {
-    $tempFile = $_FILES['files']['tmp_name'];
-    $file_name = $_FILES['files']['name'];
-    $ext = pathinfo($file_name, PATHINFO_EXTENSION);
-
-    if(filesize($tempFile) > 5000000){
-        echo 'FAILED';
-        die();
-    }
-
-    if($ext == 'jpg' || $ext == 'png' || $ext == 'JPG' || $ext == 'PNG'){
-        $new_file_name = time().'.'.$ext;
-        $targetPath = 'uploads/';  //4
-        $targetFile =  $targetPath. $new_file_name;  //5
-
-        if ($img = @imagecreatefromstring(file_get_contents($tempFile))) {
-            //$upload_result = move_uploaded_file($tempFile,$targetFile); //6
-            $server_res = \Cloudinary\Uploader::upload($tempFile, array("crop"=>"limit", "tags"=>"samples", "width"=>100, "height"=>100));
-            echo $server_res['url'];
-        }else{
-            echo 'FAILED';
-        }
-    }else{
-        echo 'FAILED';
-    }
-}
+//require 'autoload.php';
+//
+//\Cloudinary::config(array(
+//    "cloud_name" => "dajkablj5",
+//    "api_key" => "963277874729395",
+//    "api_secret" => "A9TVBKOy3TKdRgsWgXx13WMFgg8"
+//));
+//
+//$ds = DIRECTORY_SEPARATOR;  //1
+//$storeFolder = 'uploads';   //2
+//if (!empty($_FILES)) {
+//    $tempFile = $_FILES['files']['tmp_name'];
+//    $file_name = $_FILES['files']['name'];
+//    $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+//
+//    if(filesize($tempFile) > 5000000){
+//        echo 'FAILED';
+//        die();
+//    }
+//
+//    if($ext == 'jpg' || $ext == 'png' || $ext == 'JPG' || $ext == 'PNG'){
+//        $new_file_name = time().'.'.$ext;
+//        $targetPath = 'uploads/';  //4
+//        $targetFile =  $targetPath. $new_file_name;  //5
+//
+//        if ($img = @imagecreatefromstring(file_get_contents($tempFile))) {
+//            //$upload_result = move_uploaded_file($tempFile,$targetFile); //6
+//            $server_res = \Cloudinary\Uploader::upload($tempFile, array("crop"=>"limit", "tags"=>"samples", "width"=>100, "height"=>100));
+//            echo $server_res['url'];
+//        }else{
+//            echo 'FAILED';
+//        }
+//    }else{
+//        echo 'FAILED';
+//    }
+//}
